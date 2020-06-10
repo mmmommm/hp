3b7a3fa329302f9bea4d53b1c8e879d2d47b8f95
ryota kise <ryota1839@gmail.com>
2020-06-10 11:32:19 +0900

    add: errorpage fix: contacttext

--- pages/contact.vue
+++ pages/contact.vue
@@ -29,7 +29,7 @@
       <div class="flex items-center">
         <div class="w-1/3"></div>
         <div class="w-2/3">
-          <button class="shadow text-black hover:text-white bg-white hover:bg-black focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+          <button class="shadow text-black hover:text-white bg-white hover:bg-black focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded" type="button">
             送信/submit
           </button>
         </div>
@@ -37,3 +37,7 @@
     </form>
   </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+export default class contact extends Vue {}
+</script>
