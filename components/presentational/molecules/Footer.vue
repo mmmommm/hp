4e1663db79d10f68bd1f0b03ccf23fe6c32956ec
ryota kise <ryota1839@gmail.com>
2020-07-20 20:30:53 +0900

    fix: footeer

--- components/presentational/molecules/Footer.vue
+++ components/presentational/molecules/Footer.vue
@@ -5,10 +5,13 @@
     </div>
     <ul class='flex mt-8 mx-auto'>
         <h4 class='w-1/4 mt-4 ml-2 md:ml-0 md:mx-4'>CONTACT　ME</h4>
-        <li class='w-1/4 md:px-4'><nuxt-link to='/contact' ><img src='~/assets/icon/gmail.svg' alt=''></nuxt-link></li>
-        <li class='w-1/4 md:px-4'><a href='https://github.com/mmmommm/' alt='' target='_blank'><img src='~/assets/icon/github.jpg' alt=''></a></li>
-        <li class='w-1/4 md:px-4'><a href='https://twitter.com/kissessenose' target='_blank'><img src='~/assets/icon/twitter.png' alt=''></a></li>
+        <li class='w-1/4 md:px-4'><nuxt-link to='/contact' ><img src='~/assets/icon/gmail.svg' alt='メールのアイコン' class='hover:opacity-75'></nuxt-link></li>
+        <li class='w-1/4 md:px-4'><a href='https://github.com/mmmommm/' alt='' target='_blank'><img src='~/assets/icon/github.jpg' alt='githubのアイコン' class='hover:opacity-75'></a></li>
+        <li class='w-1/4 md:px-4'><a href='https://twitter.com/kissessenose' target='_blank'><img src='~/assets/icon/twitter.png' alt='twitterのアイコン' class='hover:opacity-75'></a></li>
     </ul>
+    <footer class="text-white text-center pt-6">
+        Copyright©︎2020 KISSEPORTFOLIO All Rights Reserved
+    </footer>
   </section>
 </template>
 
