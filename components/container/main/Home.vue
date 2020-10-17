6f509b19da1b71a61c6aabeb5f335fc13b96cd82
mmmommm <ryota1839@gmail.com>
2020-10-17 15:24:25 +0900

    変更忘れ

--- components/container/main/Home.vue
+++ components/container/main/Home.vue
@@ -6,7 +6,7 @@
         <img src='~/assets/image/Bicycle2.jpeg' alt='父母ヶ浜で撮った自転車と一緒に撮った画像' class='h-1/2 homeImage2'>
       </div>
       <div class='md:hidden'>
-        <img src='~assets/image/smhome.jpeg' data-src='~assets/image/smsize.jpeg' alt='父母ヶ浜で撮った自転車と一緒に撮った画像' class='smimage'>
+        <img src='~assets/image/smsize.jpeg' data-src='~assets/image/smsize.jpeg' alt='父母ヶ浜で撮った自転車と一緒に撮った画像' class='smimage'>
       </div>
     </div>
     <div class='md:w-1/2 text-center'>
